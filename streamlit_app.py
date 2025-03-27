154d81ec470d0365e17099dfa4b831b143a149f0
Shak-snow <coolest_sharhan@yahoo.com>
2025-03-28 10:35:26 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,8 @@ if ingredient_list:
     ingredients_string = ''
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
-
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(INGREDIENTS,NAME_ON_ORDER)
             values ('""" + ingredients_string +  """','""" +name_on_order+ """')"""
@@ -49,12 +50,10 @@ if ingredient_list:
          st.success('Your Smoothie is ordered! ' + name_on_order, icon="✅")
 
 
-#NEW SECTION TO DISPLAY SMOOTHIEFROOT NUTRITION INFORMATION
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
+
 
 
 
